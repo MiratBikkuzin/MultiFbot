beed7600ec57be5ce19ac9e0a23d917595ccd12c
Mirat <mirat1909@gmail.com>
2023-08-17 16:33:37 +0500

    Version four. Removing many buttons from the menu and adding proxy button

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 from currency_converter import CurrencyConverter
 
 
-bot = TeleBot('6205240292:AAGDlOC9k8eyseDB_XguUSgnkVwoJRzLhlI')
+bot = TeleBot('')
 currency = CurrencyConverter()
 amount = 0
 
@@ -23,8 +23,8 @@ def currency_start(message):
 
 
 def convert_summa(message_conv):
-    global amount
 
+    global amount
     try:
         amount = int(message_conv.text.strip())
 
@@ -41,11 +41,9 @@ def convert_summa(message_conv):
         btn_conv2 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
         btn_conv3 = types.InlineKeyboardButton('RUB/USD', callback_data='rub/usd')
         btn_conv4 = types.InlineKeyboardButton('RUB/EUR', callback_data='rub/eur')
-        btn_conv5 = types.InlineKeyboardButton('USD/RUB', callback_data='usd/rub')
-        btn_conv6 = types.InlineKeyboardButton('EUR/RUB', callback_data='eur/rub')
         btn_back = types.InlineKeyboardButton('Вернуться назад', callback_data='back')
 
-        markup_conv.add(btn_conv1, btn_conv2, btn_conv3, btn_conv4, btn_conv5, btn_conv6, btn_back)
+        markup_conv.add(btn_conv1, btn_conv2, btn_conv3, btn_conv4, btn_back)
         bot.send_message(message_conv.chat.id, 'Выберите пару валют для конвертации', reply_markup=markup_conv)
 
     else:
@@ -59,8 +57,8 @@ def callback_conv(call_conv):
 
     if call_conv.data != 'back':
 
-        values = call_conv.data.upper().split('/')
-        res = currency.convert(amount, values[0], values[1])
+        first, second = call_conv.data.upper().split('/')
+        res = currency.convert(amount, first, second)
         bot.send_message(call_conv.message.chat.id, f'Результат конвертации: {round(res, 2)}')
 
     else:
@@ -71,45 +69,23 @@ def callback_conv(call_conv):
 def start(message):
 
     markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton('Информация о боте')
-    item2 = types.KeyboardButton('Чат-бот ChatGPT')
+    item1 = types.KeyboardButton('Прокси-Proxy')
     item_random = types.KeyboardButton('Рандомное число')
 
-    markup1.add(item1, item2, item_random)
+    markup1.add(item1, item_random)
 
     bot.send_message(message.chat.id, 'Держи меню, {0.first_name}'.format(message.from_user), reply_markup=markup1)
 
 
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
-    if message.text == 'Информация о боте':
-
-        markup3 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton('О боте')
-        back = types.KeyboardButton('Вернуться назад')
-
-        markup3.add(item1, back)
-
-        bot.send_message(message.chat.id, 'Вы выбрали: Информация о боте', reply_markup=markup3)
-
-    elif message.text == 'О боте':
-        bot.send_message(message.chat.id, 'Привет, {0.first_name}! Это бот, который будет обновляться ежедневно. Может '
-                                          'быть сейчас он не такой интересный, как тебе хотелось бы, но в скором '
-                                          'времени'
-                                          ' он будет лучше'.format(message.from_user))
-
-    elif message.text == 'Чат-бот ChatGPT':
-
-        markup4 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        reference = types.KeyboardButton('Ссылка на ChatGPT')
-        back = types.KeyboardButton('Вернуться назад')
-
-        markup4.add(reference, back)
-
-        bot.send_message(message.chat.id, 'Вы выбрали: Чат-бот ChatGPT', reply_markup=markup4)
-
-    elif message.text == 'Ссылка на ChatGPT':
-        bot.send_message(message.chat.id, 'https://t.me/GPTtg_bot?start=2117781172')
+    if message.text == 'Прокси-Proxy':
+        bot.send_message(message.chat.id, 'Если тебе нужен хороший прокси, то я могу тебе его посоветовать '
+                                          'на сайте https://proxy6.net/?r=522143. А это подарок от меня '
+                                          '"Промокод на скидку 5%: yKeUJqzh8z".'
+                                          '(If you need a good proxy, I can recommend you '
+                                          'one at https://proxy6.net/?r=522143. And this is a gift from me '
+                                          '"5% discount promo code: yKeUJqzh8z".)')
 
     elif message.text == 'Рандомное число':
         n = randint(1, 100)
@@ -121,11 +97,10 @@ def bot_message(message):
     elif message.text == 'Вернуться назад':
 
         markup_back = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton('Информация о боте')
-        item2 = types.KeyboardButton('Чат-бот ChatGPT')
+        item1 = types.KeyboardButton('Прокси-Proxy')
         item_random = types.KeyboardButton('Рандомное число')
 
-        markup_back.add(item1, item2, item_random)
+        markup_back.add(item1, item_random)
 
         bot.send_message(message.chat.id, 'Вы выбрали: Вернуться назад', reply_markup=markup_back)
 
